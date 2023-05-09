252800285f1d4c821ce2b3b016bc32b5a2a97bd1
Lik206 <90332392+Lik206@users.noreply.github.com>
2023-05-09 12:25:58 +0700

    Add files via upload

---
+++ moduleBiodata.js
@@ -0,0 +1,9 @@
+nama = 'Abdul Cholik'
+tempatLahir = 'Majalengka'
+tanggalLahir = '2 Juni 2002'
+alamat = 'Gg. Raden Semar 5 JL. Sukahaji-Maja'
+
+console.log(`Nama: ${nama}`);
+console.log(`Tempat Lahir: ${tempatLahir}`);
+console.log(`Tanggal Lahir: ${tanggalLahir}`);
+console.log(`Alamat: ${alamat}`);
