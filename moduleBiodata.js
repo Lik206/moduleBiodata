757a9b77572a7bd3cc6f7edd3528f4b6fbd7c5ac
Lik206 <90332392+Lik206@users.noreply.github.com>
2023-05-09 12:27:38 +0700

    Update moduleBiodata.js

--- moduleBiodata.js
+++ moduleBiodata.js
@@ -3,7 +3,7 @@ tempatLahir = 'Majalengka'
 tanggalLahir = '2 Juni 2002'
 alamat = 'Gg. Raden Semar 5 JL. Sukahaji-Maja'
 
-console.log(`Nama: ${nama}`);
-console.log(`Tempat Lahir: ${tempatLahir}`);
-console.log(`Tanggal Lahir: ${tanggalLahir}`);
-console.log(`Alamat: ${alamat}`);
+console.log(`Nama: ${nama}`)
+console.log(`Tempat Lahir: ${tempatLahir}`)
+console.log(`Tanggal Lahir: ${tanggalLahir}`)
+console.log(`Alamat: ${alamat}`)
